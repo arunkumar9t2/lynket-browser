1c41e0776b92232c8c5b68955c6f7f7fef00d52c
Arunkumar <arunk.beece@gmail.com>
2016-02-28 18:13:38 +0530

    Secondary browser receiver enhanced to handle more use cases such as Component Name changed, browser uninstalled or any other exception while launching the activity.

--- app/src/main/java/arun/com/chromer/receivers/SecondaryBrowserReceiver.java
+++ app/src/main/java/arun/com/chromer/receivers/SecondaryBrowserReceiver.java
@@ -1,13 +1,20 @@
 package arun.com.chromer.receivers;
 
+import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.widget.Toast;
 
+import java.util.List;
+
+import arun.com.chromer.R;
 import arun.com.chromer.util.Preferences;
+import timber.log.Timber;
 
 public class SecondaryBrowserReceiver extends BroadcastReceiver {
     public SecondaryBrowserReceiver() {
@@ -18,23 +25,64 @@ public class SecondaryBrowserReceiver extends BroadcastReceiver {
         String url = intent.getDataString();
 
         if (url != null) {
-            Intent activityIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            // TODO Fix component missing cases
-            activityIntent.setComponent(ComponentName.unflattenFromString(
-                    Preferences.secondaryBrowserComponent(context)));
-            try {
-                context.startActivity(activityIntent);
-            } catch (Exception e) {
-                Toast.makeText(context, "Something went wrong, try again!", Toast.LENGTH_SHORT).show();
-                Intent defaultIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            Intent webIntentExplicit = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            webIntentExplicit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                Intent chooserIntent = Intent.createChooser(defaultIntent, "Open with..");
-                chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(chooserIntent);
-            }
+            String componentFlatten = Preferences.secondaryBrowserComponent(context);
+
+            if (componentFlatten != null) {
+                ComponentName cN = ComponentName.unflattenFromString(componentFlatten);
+                webIntentExplicit.setComponent(cN);
+
+                try {
+                    context.startActivity(webIntentExplicit);
+                } catch (ActivityNotFoundException e) {
+                    launchComponentWithIteration(context, url);
+                }
+            } else showChooser(context, url);
         } else {
-            Toast.makeText(context, "Something went wrong, try again!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, context.getString(R.string.unsupported_link), Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void launchComponentWithIteration(Context context, String url) {
+        Timber.d("Attempting to launch activity with iteration");
+
+        Intent webIntentImplicit = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        webIntentImplicit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        List<ResolveInfo> resolvedActivityList = context.getPackageManager()
+                .queryIntentActivities(webIntentImplicit, PackageManager.MATCH_ALL);
+
+        String secondaryPackage = Preferences.secondaryBrowserPackage(context);
+
+        if (secondaryPackage != null) {
+            boolean found = false;
+            for (ResolveInfo info : resolvedActivityList) {
+                if (info.activityInfo.packageName.equalsIgnoreCase(secondaryPackage)) {
+                    found = true;
+
+                    ComponentName componentName = new ComponentName(info.activityInfo.packageName,
+                            info.activityInfo.name);
+                    webIntentImplicit.setComponent(componentName);
+
+                    // This will be the new component, so write it to preferences
+                    Preferences.secondaryBrowserComponent(context, componentName.flattenToString());
+
+                    context.startActivity(webIntentImplicit);
+                }
+            }
+            if (!found) showChooser(context, url);
+        }
+    }
+
+    private void showChooser(Context context, String url) {
+        Timber.d("Falling back to intent chooser");
+
+        Toast.makeText(context, context.getString(R.string.unxp_err), Toast.LENGTH_SHORT).show();
+        Intent implicitViewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+        Intent chooserIntent = Intent.createChooser(implicitViewIntent, context.getString(R.string.open_with));
+        chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(chooserIntent);
     }
 }
