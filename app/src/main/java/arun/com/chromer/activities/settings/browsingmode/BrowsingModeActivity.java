d417d597d0304027aaa32866aba39dfe1ef83764
Arunkumar <arunk.beece@gmail.com>
2017-02-21 02:26:39 +0530

    Runtime permissions handling

--- app/src/main/java/arun/com/chromer/activities/settings/browsingmode/BrowsingModeActivity.java
+++ app/src/main/java/arun/com/chromer/activities/settings/browsingmode/BrowsingModeActivity.java
@@ -1,6 +1,13 @@
 package arun.com.chromer.activities.settings.browsingmode;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +25,8 @@ public class BrowsingModeActivity extends SubActivity implements BrowsingModeAda
     Toolbar toolbar;
     @BindView(R.id.browsing_mode_list_view)
     RecyclerView browsingModeListView;
+    @BindView(R.id.coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
     private BrowsingModeAdapter adapter;
 
     @Override
@@ -44,6 +53,25 @@ public class BrowsingModeActivity extends SubActivity implements BrowsingModeAda
     @Override
     public void onModeClicked(int position, View view) {
         Preferences.get(this).webHeads(position == 1);
+        if (position == 1) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Preferences.get(this).webHeads(false);
+                    final Snackbar snackbar = Snackbar.make(coordinatorLayout, R.string.overlay_permission_content, Snackbar.LENGTH_INDEFINITE);
+                    snackbar.setAction(R.string.grant, new View.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onClick(View v) {
+                            snackbar.dismiss();
+                            final Uri pkgUri = Uri.parse("package:" + BrowsingModeActivity.this.getPackageName());
+                            final Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, pkgUri);
+                            startActivityForResult(intent, 0);
+                        }
+                    });
+                    snackbar.show();
+                } else Preferences.get(this).webHeads(true);
+            } else Preferences.get(this).webHeads(true);
+        } else Preferences.get(this).webHeads(false);
         adapter.notifyDataSetChanged();
     }
 }
