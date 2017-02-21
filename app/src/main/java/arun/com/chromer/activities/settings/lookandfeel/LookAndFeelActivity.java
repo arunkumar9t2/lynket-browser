b40865a26014445aeddaf3f6ad492a6968377a25
Arunkumar <arunk.beece@gmail.com>
2017-02-22 04:09:58 +0530

    Removed un needed toggles from web head section in look and feel. Added error view to tell that web head section will not be available during slide over mode.

--- app/src/main/java/arun/com/chromer/activities/settings/lookandfeel/LookAndFeelActivity.java
+++ app/src/main/java/arun/com/chromer/activities/settings/lookandfeel/LookAndFeelActivity.java
@@ -1,6 +1,7 @@
 package arun.com.chromer.activities.settings.lookandfeel;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
@@ -8,12 +9,16 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.afollestad.materialdialogs.color.ColorChooserDialog;
 
 import arun.com.chromer.R;
 import arun.com.chromer.activities.SnackHelper;
+import arun.com.chromer.activities.settings.Preferences;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -22,12 +27,13 @@ import static arun.com.chromer.shared.Constants.ACTION_WEBHEAD_COLOR_SET;
 import static arun.com.chromer.shared.Constants.EXTRA_KEY_TOOLBAR_COLOR;
 import static arun.com.chromer.shared.Constants.EXTRA_KEY_WEBHEAD_COLOR;
 
-public class LookAndFeelActivity extends AppCompatActivity implements ColorChooserDialog.ColorCallback, SnackHelper {
+public class LookAndFeelActivity extends AppCompatActivity implements ColorChooserDialog.ColorCallback, SnackHelper, SharedPreferences.OnSharedPreferenceChangeListener {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.coordinator_layout)
     CoordinatorLayout coordinatorLayout;
-    private PersonalizationPreferenceFragment preferenceFragment;
+    @BindView(R.id.error)
+    TextView error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +44,32 @@ public class LookAndFeelActivity extends AppCompatActivity implements ColorChoos
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        preferenceFragment = PersonalizationPreferenceFragment.newInstance();
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.toolbar_options_preferences_container, preferenceFragment)
+                .replace(R.id.toolbar_options_preferences_container, PersonalizationPreferenceFragment.newInstance())
                 .replace(R.id.web_head_options_preferences_container, WebHeadPreferenceFragment.newInstance())
                 .commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        showHideErrorView();
+    }
+
+    private void showHideErrorView() {
+        if (!Preferences.get(this).webHeads()) {
+            error.setVisibility(View.VISIBLE);
+        } else {
+            error.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
     }
 
     @Override
@@ -70,5 +96,12 @@ public class LookAndFeelActivity extends AppCompatActivity implements ColorChoos
     @Override
     public void snackLong(@NonNull String message) {
         Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (Preferences.WEB_HEAD_ENABLED.equalsIgnoreCase(key)) {
+            showHideErrorView();
+        }
     }
 }
--- app/src/main/java/arun/com/chromer/activities/settings/lookandfeel/WebHeadPreferenceFragment.java
+++ app/src/main/java/arun/com/chromer/activities/settings/lookandfeel/WebHeadPreferenceFragment.java
@@ -1,22 +1,14 @@
 package arun.com.chromer.activities.settings.lookandfeel;
 
-import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.SwitchPreferenceCompat;
 
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.color.ColorChooserDialog;
 import com.mikepenz.community_material_typeface_library.CommunityMaterial;
 import com.mikepenz.iconics.IconicsDrawable;
@@ -25,16 +17,10 @@ import arun.com.chromer.R;
 import arun.com.chromer.activities.settings.Preferences;
 import arun.com.chromer.activities.settings.preferences.BasePreferenceFragment;
 import arun.com.chromer.activities.settings.widgets.ColorPreference;
-import arun.com.chromer.activities.settings.widgets.IconCheckboxPreference;
 import arun.com.chromer.activities.settings.widgets.IconListPreference;
-import arun.com.chromer.activities.settings.widgets.IconSwitchPreference;
 import arun.com.chromer.shared.Constants;
-import arun.com.chromer.util.Utils;
 
-import static arun.com.chromer.activities.settings.Preferences.AGGRESSIVE_LOADING;
-import static arun.com.chromer.activities.settings.Preferences.MERGE_TABS_AND_APPS;
 import static arun.com.chromer.activities.settings.Preferences.WEB_HEADS_COLOR;
-import static arun.com.chromer.activities.settings.Preferences.WEB_HEAD_CLOSE_ON_OPEN;
 import static arun.com.chromer.activities.settings.Preferences.WEB_HEAD_ENABLED;
 import static arun.com.chromer.activities.settings.Preferences.WEB_HEAD_SIZE;
 import static arun.com.chromer.activities.settings.Preferences.WEB_HEAD_SPAWN_LOCATION;
@@ -51,10 +37,7 @@ public class WebHeadPreferenceFragment extends BasePreferenceFragment implements
     private final IntentFilter webHeadColorFilter = new IntentFilter(Constants.ACTION_WEBHEAD_COLOR_SET);
 
     private ColorPreference webHeadColor;
-    private IconSwitchPreference webHeadSwitch;
     private IconListPreference spawnLocation;
-    private IconCheckboxPreference closeOnOpen;
-    private IconCheckboxPreference aggressiveLoading;
     private IconListPreference webHeadSize;
 
     public WebHeadPreferenceFragment() {
@@ -74,9 +57,7 @@ public class WebHeadPreferenceFragment extends BasePreferenceFragment implements
         addPreferencesFromResource(R.xml.webhead_preferences);
         init();
         setIcons();
-        setUpWebHeadSwitch();
         setupWebHeadColorPreference();
-        setupAggressivePreference();
     }
 
 
@@ -86,9 +67,6 @@ public class WebHeadPreferenceFragment extends BasePreferenceFragment implements
         registerReceiver(colorSelectionReceiver, webHeadColorFilter);
         updatePreferenceStates(WEB_HEAD_ENABLED);
         updatePreferenceSummary(SUMMARY_GROUP);
-        if (!Utils.isLollipopAbove()) {
-            aggressiveLoading.setVisible(false);
-        }
     }
 
     @Override
@@ -101,20 +79,12 @@ public class WebHeadPreferenceFragment extends BasePreferenceFragment implements
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         updatePreferenceStates(key);
         updatePreferenceSummary(key);
-        if (key.equalsIgnoreCase(MERGE_TABS_AND_APPS)) {
-            if (!Preferences.get(getContext()).mergeTabs()) {
-                aggressiveLoading.setChecked(false);
-            }
-        }
     }
 
     private void init() {
-        webHeadSwitch = (IconSwitchPreference) findPreference(WEB_HEAD_ENABLED);
         webHeadColor = (ColorPreference) findPreference(WEB_HEADS_COLOR);
         spawnLocation = (IconListPreference) findPreference(WEB_HEAD_SPAWN_LOCATION);
         webHeadSize = (IconListPreference) findPreference(WEB_HEAD_SIZE);
-        closeOnOpen = (IconCheckboxPreference) findPreference(WEB_HEAD_CLOSE_ON_OPEN);
-        aggressiveLoading = (IconCheckboxPreference) findPreference(AGGRESSIVE_LOADING);
     }
 
 
@@ -124,24 +94,12 @@ public class WebHeadPreferenceFragment extends BasePreferenceFragment implements
                 .icon(CommunityMaterial.Icon.cmd_palette)
                 .color(materialLight)
                 .sizeDp(24));
-        webHeadSwitch.setIcon(new IconicsDrawable(getActivity())
-                .icon(CommunityMaterial.Icon.cmd_chart_bubble)
-                .color(materialLight)
-                .sizeDp(24));
         spawnLocation.setIcon(new IconicsDrawable(getActivity())
                 .icon(CommunityMaterial.Icon.cmd_code_tags)
                 .color(materialLight)
                 .sizeDp(24));
         webHeadSize.setIcon(new IconicsDrawable(getActivity())
                 .icon(CommunityMaterial.Icon.cmd_crop_free)
-                .color(materialLight)
-                .sizeDp(24));
-        closeOnOpen.setIcon(new IconicsDrawable(getActivity())
-                .icon(CommunityMaterial.Icon.cmd_close_circle_outline)
-                .color(materialLight)
-                .sizeDp(24));
-        aggressiveLoading.setIcon(new IconicsDrawable(getActivity())
-                .icon(CommunityMaterial.Icon.cmd_fast_forward)
                 .color(materialLight)
                 .sizeDp(24));
     }
@@ -162,69 +120,10 @@ public class WebHeadPreferenceFragment extends BasePreferenceFragment implements
         });
     }
 
-    private void setupAggressivePreference() {
-        aggressiveLoading.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                if (((Boolean) newValue) && !Preferences.get(getContext()).mergeTabs()) {
-                    new MaterialDialog.Builder(getActivity())
-                            .title(R.string.aggresive_dia_title)
-                            .content(R.string.aggresive_dia_content)
-                            .positiveText(android.R.string.ok)
-                            .show();
-                    Preferences.get(getContext()).mergeTabs(true);
-                }
-                return true;
-            }
-        });
-    }
-
-    private void setUpWebHeadSwitch() {
-        webHeadSwitch.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                final SwitchPreferenceCompat switchCompat = (SwitchPreferenceCompat) preference;
-                final boolean isChecked = switchCompat.isChecked();
-                if (isChecked) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (!Settings.canDrawOverlays(getActivity())) {
-                            // Don't check the switch until permission is granted
-                            switchCompat.setChecked(false);
-                            new MaterialDialog.Builder(getActivity())
-                                    .title(R.string.permission_required)
-                                    .content(R.string.overlay_permission_content)
-                                    .positiveText(R.string.grant)
-                                    .onPositive(new MaterialDialog.SingleButtonCallback() {
-                                        @TargetApi(Build.VERSION_CODES.M)
-                                        @Override
-                                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                                            dialog.dismiss();
-                                            final Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                                                    Uri.parse("package:" + getActivity().getPackageName()));
-                                            getActivity().startActivityForResult(intent, 0);
-                                        }
-                                    })
-                                    .show();
-                        } else {
-                            snackLong(getString(R.string.web_heads_enabled));
-                        }
-                    } else snackLong(getString(R.string.web_heads_enabled));
-                }
-                return false;
-            }
-        });
-    }
-
     private void updatePreferenceStates(String key) {
         if (key.equalsIgnoreCase(WEB_HEAD_ENABLED)) {
             final boolean webHeadsEnabled = Preferences.get(getContext()).webHeads();
-            enableDisablePreference(webHeadsEnabled,
-                    WEB_HEAD_SPAWN_LOCATION,
-                    WEB_HEADS_COLOR,
-                    WEB_HEAD_CLOSE_ON_OPEN,
-                    WEB_HEAD_SIZE,
-                    AGGRESSIVE_LOADING
-            );
+            enableDisablePreference(webHeadsEnabled, SUMMARY_GROUP);
         }
     }
 
