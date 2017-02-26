66a7a5525ce3804eeb5a7c0bad61196a1753f33b
Arunkumar <arunk.beece@gmail.com>
2017-02-26 22:18:05 +0530

    Better error handling when article could not be parsed.

--- app/src/main/java/arun/com/chromer/activities/browsing/article/ChromerArticleActivity.java
+++ app/src/main/java/arun/com/chromer/activities/browsing/article/ChromerArticleActivity.java
@@ -1,7 +1,19 @@
 package arun.com.chromer.activities.browsing.article;
 
+import android.content.Intent;
+
+import arun.com.chromer.activities.CustomTabActivity;
 import xyz.klinker.android.article.ArticleActivity;
 
 public class ChromerArticleActivity extends ArticleActivity {
 
+    @Override
+    protected void onArticleLoadingFailed(Throwable throwable) {
+        super.onArticleLoadingFailed(throwable);
+        // Start custom tab activity on the same task, silently killing this activity.
+        final Intent customTabActivity = new Intent(this, CustomTabActivity.class);
+        customTabActivity.setData(getIntent().getData());
+        startActivity(customTabActivity);
+        finish();
+    }
 }
