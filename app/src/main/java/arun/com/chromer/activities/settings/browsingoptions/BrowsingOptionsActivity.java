f7817f6239d35c993e7132416cd1dc0b475b8ca3
Arunkumar <arunk.beece@gmail.com>
2017-03-09 06:59:26 +0530

    Bottom bar explanation

--- app/src/main/java/arun/com/chromer/activities/settings/browsingoptions/BrowsingOptionsActivity.java
+++ app/src/main/java/arun/com/chromer/activities/settings/browsingoptions/BrowsingOptionsActivity.java
@@ -1,20 +1,34 @@
 package arun.com.chromer.activities.settings.browsingoptions;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.PreferenceManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import arun.com.chromer.R;
@@ -50,6 +64,8 @@ public class BrowsingOptionsActivity extends SubActivity implements SnackHelper,
     BottomSheetLayout bottomSheetLayout;
     @BindView(R.id.error)
     TextView error;
+    @BindView(R.id.bottom_bar_action_list)
+    public RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +82,8 @@ public class BrowsingOptionsActivity extends SubActivity implements SnackHelper,
                 .replace(R.id.web_head_fragment_container, WebHeadOptionsFragment.newInstance())
                 .replace(R.id.bottom_bar_preference_fragment_container, BottomBarPreferenceFragment.newInstance())
                 .commit();
+
+        initBottomActions();
     }
 
     @Override
@@ -73,6 +91,11 @@ public class BrowsingOptionsActivity extends SubActivity implements SnackHelper,
         super.onResume();
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
         showHideErrorView();
+    }
+
+    private void initBottomActions() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new BottomActionsAdapter(this));
     }
 
     private void showHideErrorView() {
@@ -178,5 +201,83 @@ public class BrowsingOptionsActivity extends SubActivity implements SnackHelper,
     @Override
     public void snackLong(@NonNull String message) {
         Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    public static class BottomActionsAdapter extends RecyclerView.Adapter<BottomActionsAdapter.BottomActionHolder> {
+        private static final String NEW_TAB = "NEW_TAB";
+        private static final String SHARE = "SHARE";
+        private static final String MINIMIZE = "MINIMIZE";
+        private final Context context;
+        private final List<String> items = new LinkedList<>();
+
+        public BottomActionsAdapter(Context context) {
+            this.context = context;
+            if (Utils.isLollipopAbove()) {
+                items.add(NEW_TAB);
+            }
+            items.add(SHARE);
+            items.add(MINIMIZE);
+        }
+
+        @Override
+        public BottomActionHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new BottomActionHolder(LayoutInflater.from(context).inflate(R.layout.widget_bottom_bar_actions, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(BottomActionHolder holder, int position) {
+            final int iconColor = ContextCompat.getColor(context, R.color.colorAccentLighter);
+            switch (items.get(position)) {
+                case NEW_TAB:
+                    holder.icon.setImageDrawable(new IconicsDrawable(context)
+                            .icon(CommunityMaterial.Icon.cmd_plus_box)
+                            .color(iconColor)
+                            .sizeDp(18));
+                    holder.action.setText(html(R.string.open_in_new_tab_explanation));
+                    break;
+                case SHARE:
+                    holder.icon.setImageDrawable(new IconicsDrawable(context)
+                            .icon(CommunityMaterial.Icon.cmd_share_variant)
+                            .color(iconColor)
+                            .sizeDp(18));
+                    holder.action.setText(html(R.string.share_action_explanation));
+                    break;
+                case MINIMIZE:
+                    holder.icon.setImageDrawable(new IconicsDrawable(context)
+                            .icon(CommunityMaterial.Icon.cmd_flip_to_back)
+                            .color(iconColor)
+                            .sizeDp(18));
+                    holder.action.setText(html(R.string.minimize_action_explanation));
+                    break;
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        @NonNull
+        private Spanned html(@StringRes int res) {
+            final String string = context.getString(res);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                return Html.fromHtml(string, Html.FROM_HTML_MODE_LEGACY);
+            } else {
+                //noinspection deprecation
+                return Html.fromHtml(string);
+            }
+        }
+
+        class BottomActionHolder extends RecyclerView.ViewHolder {
+            @BindView(R.id.bottom_action)
+            TextView action;
+            @BindView(R.id.bottom_icon)
+            ImageView icon;
+
+            BottomActionHolder(View view) {
+                super(view);
+                ButterKnife.bind(this, view);
+            }
+        }
     }
 }
--- app/src/main/java/arun/com/chromer/activities/settings/fragments/CustomizeFragment.java
+++ app/src/main/java/arun/com/chromer/activities/settings/fragments/CustomizeFragment.java
@@ -1,33 +1,18 @@
 package arun.com.chromer.activities.settings.fragments;
 
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Html;
-import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.mikepenz.community_material_typeface_library.CommunityMaterial;
-import com.mikepenz.iconics.IconicsDrawable;
-
-import java.util.LinkedList;
-import java.util.List;
 
 import arun.com.chromer.R;
 import arun.com.chromer.activities.settings.browsingoptions.BottomBarPreferenceFragment;
+import arun.com.chromer.activities.settings.browsingoptions.BrowsingOptionsActivity;
 import arun.com.chromer.activities.settings.lookandfeel.PersonalizationPreferenceFragment;
-import arun.com.chromer.util.Utils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -68,7 +53,7 @@ public class CustomizeFragment extends Fragment {
 
     private void initBottomActions() {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new BottomActionsAdapter(getActivity()));
+        recyclerView.setAdapter(new BrowsingOptionsActivity.BottomActionsAdapter(getActivity()));
     }
 
     @Override
@@ -77,81 +62,4 @@ public class CustomizeFragment extends Fragment {
         mUnbinder.unbind();
     }
 
-    public static class BottomActionsAdapter extends RecyclerView.Adapter<BottomActionsAdapter.BottomActionHolder> {
-        private static final String NEW_TAB = "NEW_TAB";
-        private static final String SHARE = "SHARE";
-        private static final String MINIMIZE = "MINIMIZE";
-        private final Context context;
-        private final List<String> items = new LinkedList<>();
-
-        BottomActionsAdapter(Context context) {
-            this.context = context;
-            if (Utils.isLollipopAbove()) {
-                items.add(NEW_TAB);
-            }
-            items.add(SHARE);
-            items.add(MINIMIZE);
-        }
-
-        @Override
-        public BottomActionHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new BottomActionHolder(LayoutInflater.from(context).inflate(R.layout.widget_bottom_bar_actions, parent, false));
-        }
-
-        @Override
-        public void onBindViewHolder(BottomActionHolder holder, int position) {
-            final int iconColor = ContextCompat.getColor(context, R.color.colorAccentLighter);
-            switch (items.get(position)) {
-                case NEW_TAB:
-                    holder.icon.setImageDrawable(new IconicsDrawable(context)
-                            .icon(CommunityMaterial.Icon.cmd_plus_box)
-                            .color(iconColor)
-                            .sizeDp(18));
-                    holder.action.setText(html(R.string.open_in_new_tab_explanation));
-                    break;
-                case SHARE:
-                    holder.icon.setImageDrawable(new IconicsDrawable(context)
-                            .icon(CommunityMaterial.Icon.cmd_share_variant)
-                            .color(iconColor)
-                            .sizeDp(18));
-                    holder.action.setText(html(R.string.share_action_explanation));
-                    break;
-                case MINIMIZE:
-                    holder.icon.setImageDrawable(new IconicsDrawable(context)
-                            .icon(CommunityMaterial.Icon.cmd_flip_to_back)
-                            .color(iconColor)
-                            .sizeDp(18));
-                    holder.action.setText(html(R.string.minimize_action_explanation));
-                    break;
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            return items.size();
-        }
-
-        @NonNull
-        private Spanned html(@StringRes int res) {
-            final String string = context.getString(res);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                return Html.fromHtml(string, Html.FROM_HTML_MODE_LEGACY);
-            } else {
-                //noinspection deprecation
-                return Html.fromHtml(string);
-            }
-        }
-
-        class BottomActionHolder extends RecyclerView.ViewHolder {
-            @BindView(R.id.bottom_action)
-            TextView action;
-            @BindView(R.id.bottom_icon)
-            ImageView icon;
-
-            BottomActionHolder(View view) {
-                super(view);
-                ButterKnife.bind(this, view);
-            }
-        }
-    }
 }
