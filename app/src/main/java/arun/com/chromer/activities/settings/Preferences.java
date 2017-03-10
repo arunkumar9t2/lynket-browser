e14731376067e0ca43a390e8e333d635de20caa6
Arunkumar <arunk.beece@gmail.com>
2017-03-10 10:12:22 +0530

    Removing unneeded drawables and changes first run key

--- app/src/main/java/arun/com/chromer/activities/settings/Preferences.java
+++ app/src/main/java/arun/com/chromer/activities/settings/Preferences.java
@@ -47,7 +47,7 @@ public class Preferences {
     private static final String WEB_HEAD_FAVICON = "webhead_favicons_pref";
     private static final String BLACKLIST = "blacklist_preference";
     private static final String PREFERRED_PACKAGE = "preferred_package";
-    private static final String FIRST_RUN = "firstrun_2";
+    private static final String FIRST_RUN = "firstrun_3";
     private static final String SECONDARY_PREF = "secondary_preference";
     private static final String FAV_SHARE_PREF = "fav_share_preference";
     public static final String DYNAMIC_COLOR_APP = "dynamic_color_app";
