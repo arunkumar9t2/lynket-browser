1f0498062c06ae9aa0dbd5c19f7c9ca6a59f9bc4
Arunkumar <arunk.beece@gmail.com>
2017-12-10 19:32:24 +0530

    Delete unused class. Remove size from request to glide as it was causing valid favicon check to fail since Glide upscales the image.

--- app/src/main/java/arun/com/chromer/util/glide/favicon/WebsiteDecoder.kt
+++ app/src/main/java/arun/com/chromer/util/glide/favicon/WebsiteDecoder.kt
@@ -54,7 +54,7 @@ class WebsiteDecoder(private val context: Context, private val glide: Glide) : R
     override fun decode(webSite: WebSite, width: Int, height: Int, options: Options?): Resource<Bitmap>? {
         // Try to load using Glide normally
         val websiteFavicon = try {
-            GlideApp.with(context).asBitmap().load(webSite.faviconUrl).submit(size, size).get()
+            GlideApp.with(context).asBitmap().load(webSite.faviconUrl).submit().get()
         } catch (e: Exception) {
             null
         }
