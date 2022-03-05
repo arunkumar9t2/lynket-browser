3c8f309cb7ca29427552c6bc95ce7f539b13bc2d
Arunkumar <arunkumar9t2@users.noreply.github.com>
2022-03-06 03:56:05 +0800

    Update copyright

--- android-app/lynket/src/main/java/arun/com/chromer/data/website/WebsiteRepository.kt
+++ android-app/lynket/src/main/java/arun/com/chromer/data/website/WebsiteRepository.kt
@@ -1,20 +1,21 @@
 /*
- * Lynket
  *
- * Copyright (C) 2019 Arunkumar
+ *  Lynket
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *  Copyright (C) 2022 Arunkumar
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package arun.com.chromer.data.website
